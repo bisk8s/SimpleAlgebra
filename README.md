856cc1a245356988fb08e49d13efd6f2d62ed3a4
G.Lucena <lucena_guilherme@yahoo.coma>
2018-12-13 16:06:15 -0200

    module and readme update

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # SimpleAlgebra
+
+## Input:
+```
+const SimpleAlgebra = require('simplealgebra');
+
+let expString = '(-2 + (- 0.9 - 0.1 + 3 * 3)) * (3 - 1)';
+let test = new SimpleAlgebra(expString);
+
+console.log(JSON.stringify(test, null, 4));
+```
+## Output:
+```
+{
+    "steps": [
+        "(-2+(-0.9-0.1+3*3))*(3-1)",
+        "(-2+(-0.9-0.1+9))*(3-1)",
+        "(-2+(-1+9))*(3-1)",
+        "(-2+8)*(3-1)",
+        "6*(3-1)",
+        "6*2"
+    ],
+    "operations": [
+        [
+            3,
+            "*",
+            3
+        ],
+        [
+            -0.9,
+            "-",
+            0.1
+        ],
+        [
+            -1,
+            "+",
+            9
+        ],
+        [
+            -2,
+            "+",
+            8
+        ],
+        [
+            3,
+            "-",
+            1
+        ],
+        [
+            6,
+            "*",
+            2
+        ]
+    ]
+}
+```
--- index.js
+++ index.js
@@ -1,6 +1,76 @@
-const SimpleAlgebra = require('./simpleAlgebra.js');
+/**
+ * Perform the operations inside a parenthesis first
+ * Then exponents
+ * Then multiplication and division, from left to right
+ * Then addition and subtraction, from left to right
+ */
+class SimpleAlgebra {
+	constructor(expression) {
+		this.steps = [];
+		this.operations = [];
+		this.solve(expression);
+	}
 
-let expString = '(-2 + (- 0.9 - 0.1 + 3 * 3)) * (3 - 1)';
-let test = new SimpleAlgebra(expString);
+	basicOperation(a, b, operator) {
+		a = parseFloat(a);
+		b = parseFloat(b);
+		operator = operator.toString().trim();
+		//
+		this.operations.push([a, operator, b]);
+		//
+		switch (operator) {
+			case '+':
+				return a + b;
+				break;
+			case '-':
+				return a - b;
+				break;
+			case '/':
+				return a / b;
+				break;
+			case '*':
+				return a * b;
+				break;
+		}
+	}
 
-console.log(JSON.stringify(test, null, 4));
+	solve(expression) {		
+		expression = expression.replace(/[^\d\(\)\.\+\-\*\/]/g, '');
+		this.steps.push(expression);
+		var innerExp = expression.match(/\(([^\)(]+)\)/);
+		if (innerExp) {
+			expression = innerExp.input.replace(innerExp[0], this.algebraSequence(innerExp[0]));
+		} else {
+			expression = this.algebraSequence(expression);
+		}
+		let matchResult = expression.match(/^([\d.]+)$/);
+		if (!matchResult) {
+			expression = this.solve(expression);
+		}
+		return expression;
+	}
+
+	algebraSequence(expression) {
+		let matchResult = null;
+		let operators = ['/', '*', '-', '+'];
+		operators.forEach(op => {
+			if (!matchResult) {
+				let regExp = new RegExp(`(\-*[\\d\\.]+)(\\s*\\${op}\\s*)([\\d\\.]+)`);
+				matchResult = expression.match(regExp);
+			}
+		})
+		let returnValue = expression;
+		if (matchResult) {
+			let operation = this.basicOperation(matchResult[1], matchResult[3], matchResult[2]).toString();
+			returnValue = returnValue.replace(matchResult[0], operation);
+		}
+		// removendo parenteses desnecessários
+		matchResult = returnValue.match(/\(([\d.]+)\)/);
+		if (matchResult) {
+			returnValue = matchResult[1];
+		}
+		return returnValue;
+	}
+}
+
+module.exports = SimpleAlgebra;
